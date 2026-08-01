8152fb9d4a25bdcd732b57d76877e74aaac3dbdb
J-Abitria <146140290+J-Abitria@users.noreply.github.com>
2026-08-01 13:55:58 -0700

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.py
@@ -0,0 +1,42 @@
+import math
+
+class Solution:
+    def add(self, x, y):
+        return x + y
+    
+    def sub(self, x, y):
+        return x - y
+    
+    def mul(self, x, y):
+        return x * y
+    
+    def div(self, x, y):
+        result = x / y
+
+        """
+        This condition is used to ensure truncation towards 0.
+        """
+        if result > 0: return math.floor(result)
+        
+        return math.ceil(result)
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        numStack = []
+
+        opDict = {
+            "+": self.add,
+            "-": self.sub,
+            "*": self.mul,
+            "/": self.div
+        }
+
+        for token in tokens:
+            if token == "+" or token == "-" or token == "*" or token == "/":
+                secondNum = numStack.pop(len(numStack) - 1)
+                firstNum = numStack.pop(len(numStack) - 1)
+                numStack.append(opDict[token](firstNum, secondNum))
+            else:
+                numStack.append(int(token))
+            print(numStack)
+            
+        return numStack.pop(len(numStack) - 1)
